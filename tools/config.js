e53c7759a8b23551b5bc2ade1b878ae2077bc615
kaave <abe@framelunch.jp>
2019-03-28 12:11:52 +0900

    common: add sass bundle config (default off)

--- tools/config.js
+++ tools/config.js
@@ -5,137 +5,119 @@ const ports = {
   browserSync: 9012,
   webpackDevServer: 13000,
 };
+exports.ports = ports;
 
-module.exports = {
-  ports,
-  dest: {
-    dev: '.tmp',
-    build: 'build',
+exports.dest = {
+  dev: '.tmp',
+  build: 'build',
+};
+
+// この項目に要素を追加すると[copy:[KEY_NAME]]という名称で勝手にtaskも増えます。
+exports.copy = {
+  // assets: [
+  //   'assets/**/*',
+  // ],
+  'b.assets': ['assets/**/*', '!assets/**/*.{jpg,jpeg,gif,png}'],
+};
+
+exports.image = {
+  createWebp: false,
+  src: ['assets/**/*.{jpg,jpeg,png}', 'assets/**/*.gif'],
+  // PNG形式: https://www.npmjs.com/package/imagemin-pngquant
+  png: {
+    // クオリティ 0(やり過ぎ) ~ 1(ほぼそのまま)
+    quality: [0.65, 0.8],
+    // 処理速度を指定 1(じっくり) ~ 10(最速) 5％くらい質に違いが出るらしい
+    speed: 1,
+    // ディザリングを設定 0(無効) ~ 1(最大)
+    floyd: 0,
+    // フロイド-スタインバーグ・ディザリングを無効化するか
+    // https://ja.wikipedia.org/wiki/%E3%83%95%E3%83%AD%E3%82%A4%E3%83%89-%E3%82%B9%E3%82%BF%E3%82%A4%E3%83%B3%E3%83%90%E3%83%BC%E3%82%B0%E3%83%BB%E3%83%87%E3%82%A3%E3%82%B6%E3%83%AA%E3%83%B3%E3%82%B0
+    nofs: false,
   },
-
-  // この項目に要素を追加すると[copy:[KEY_NAME]]という名称で勝手にtaskも増えます。
-  copy: {
-    // assets: [
-    //   'assets/**/*',
-    // ],
-    'b.assets': ['assets/**/*', '!assets/**/*.{jpg,jpeg,gif,png}'],
+  // JPG形式: https://www.npmjs.com/package/imagemin-mozjpeg
+  jpg: {
+    // クオリティ 0(やり過ぎ) ~ 100(ほぼそのまま)
+    quality: 80,
+    // プログレッシブjpegを作成するか falseにするとベースラインjpeg
+    progressive: true,
   },
+  // GIF形式: https://github.com/imagemin/imagemin-gifsicle#imagemingifsicleoptionsbuffer
+  gif: {
+    // 最適化レベル 1(ちょっと)-3(そこそこ)で指定
+    optimizationLevel: 3,
+  },
+  // SVG形式: https://github.com/svg/svgo#what-it-can-do
+  svg: {},
+  // WebP形式: https://github.com/imagemin/imagemin-webp#imagemin-webp-
+  webp: {
+    quality: 80,
+  },
+  gif2webp: {
+    quality: 80,
+  },
+};
 
-  image: {
-    createWebp: false,
-    src: ['assets/**/*.{jpg,jpeg,png}', 'assets/**/*.gif'],
-    // PNG形式: https://www.npmjs.com/package/imagemin-pngquant
-    png: {
-      // クオリティ 0(やり過ぎ) ~ 1(ほぼそのまま)
-      quality: [0.65, 0.8],
-      // 処理速度を指定 1(じっくり) ~ 10(最速) 5％くらい質に違いが出るらしい
-      speed: 1,
-      // ディザリングを設定 0(無効) ~ 1(最大)
-      floyd: 0,
-      // フロイド-スタインバーグ・ディザリングを無効化するか
-      // https://ja.wikipedia.org/wiki/%E3%83%95%E3%83%AD%E3%82%A4%E3%83%89-%E3%82%B9%E3%82%BF%E3%82%A4%E3%83%B3%E3%83%90%E3%83%BC%E3%82%B0%E3%83%BB%E3%83%87%E3%82%A3%E3%82%B6%E3%83%AA%E3%83%B3%E3%82%B0
-      nofs: false,
+exports.rev = {
+  src: 'build/**/*.{js,css,png,gif,jpg,jpeg,svg,eot,ttf,woff,ico}',
+  dest: 'build',
+  manifestFileName: 'manifest.json',
+  isEnable: false, // ここをtrueにすると生成ファイルにハッシュが付きます。ただし差分ビルドが死にます。
+};
+
+exports.replace = {
+  src: ['build/index.html', 'build/**/*.{js,css,html}'],
+  dest: 'build',
+};
+
+exports.view = {
+  src: ['src/views/**/*.ejs', '!src/views/**/_*'],
+  watch: ['src/views/**/*.ejs'],
+};
+
+const useScss = false;
+exports.style = {
+  useScss,
+  src: [`src/**/*.${useScss ? 's' : ''}css`, '!src/**/_*', '!src/components/**/*', '!src/assets/**/*'],
+  watch: [`src/**/*.${useScss ? 's' : ''}css`, `src/components/**/*.${useScss ? 's' : ''}css`],
+  urlOption: { filter: ['./**/*'], url: 'inline' },
+  autoprefixerOption: { grid: true },
+  cssnanoOption: {
+    safe: true,
+    calc: false,
+  },
+};
+
+exports.script = {
+  src: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/_*', '!src/components/**/*', '!src/assets/**/*'],
+  watch: ['src/**/*.{js,jsx,ts,tsx}', 'src/components/**/*.{js,jsx,ts,tsx}'],
+  entry: {
+    'js/index': './src/js/index.js',
+  },
+};
+
+exports.spreadsheet = {
+  // Spreadsheet ID URLから引っ張る
+  id: '1SOa3TeSRJdpcuoTkoRWuhRZDEaFEzt5CJQFThPgv0Ec',
+  output: './src/spreadsheet.json',
+  sheets: [
+    // 以下サンプル なんだか知らないけど列名は小文字になる模様
+    {
+      key: 'users',
+      sheetTitle: 'ユーザ一覧',
+      rows: [['id', 'id'], ['name', '名前'], ['age', '年齢']],
     },
-    // JPG形式: https://www.npmjs.com/package/imagemin-mozjpeg
-    jpg: {
-      // クオリティ 0(やり過ぎ) ~ 100(ほぼそのまま)
-      quality: 80,
-      // プログレッシブjpegを作成するか falseにするとベースラインjpeg
-      progressive: true,
+    {
+      key: 'groups',
+      sheetTitle: 'グループ',
+      rows: [['key', 'キー'], ['name', '名前']],
     },
-    // GIF形式: https://github.com/imagemin/imagemin-gifsicle#imagemingifsicleoptionsbuffer
-    gif: {
-      // 最適化レベル 1(ちょっと)-3(そこそこ)で指定
-      optimizationLevel: 3,
-    },
-    // SVG形式: https://github.com/svg/svgo#what-it-can-do
-    svg: {},
-    // WebP形式: https://github.com/imagemin/imagemin-webp#imagemin-webp-
-    webp: {
-      quality: 80,
-    },
-    gif2webp: {
-      quality: 80,
-    },
-  },
+  ],
+};
 
-  rev: {
-    src: 'build/**/*.{js,css,png,gif,jpg,jpeg,svg,eot,ttf,woff,ico}',
-    dest: 'build',
-    manifestFileName: 'manifest.json',
-    isEnable: false, // ここをtrueにすると生成ファイルにハッシュが付きます。ただし差分ビルドが死にます。
-  },
-
-  revReplace: {
-    src: ['build/index.html', 'build/**/*.{js,css,html}'],
-    dest: 'build',
-  },
-
-  view: {
-    src: ['src/views/**/*.ejs', '!src/views/**/_*'],
-    watch: ['src/views/**/*.ejs'],
-    rename(path) {
-      // TODO: ちょっと複雑感あるので、一旦コメントアウト
-      // if (path.basename === 'index') {
-      //   return;
-      // }
-      // let basename = 'index';
-      // let dirname = `${path.dirname}/`;
-      // dirname += path.basename.split('.').reduce((str, item) => {
-      //   if (item.charAt(0) === '_') {
-      //     basename = item.substr(1);
-      //   } else {
-      //     str += `${item}/`;
-      //   }
-      //   return str;
-      // }, '');
-      // path.basename = basename;
-      // path.dirname = dirname;
-    },
-  },
-
-  style: {
-    src: ['src/**/*.css', '!src/**/_*', '!src/components/**/*', '!src/assets/**/*'],
-    watch: ['src/**/*.css', 'src/components/**/*.css'],
-    urlOption: { filter: ['./**/*'], url: 'inline' },
-    autoprefixerOption: { grid: true },
-    cssnanoOption: {
-      safe: true,
-      calc: false,
-    },
-  },
-
-  script: {
-    src: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/_*', '!src/components/**/*', '!src/assets/**/*'],
-    watch: ['src/**/*.{js,jsx,ts,tsx}', 'src/components/**/*.{js,jsx,ts,tsx}'],
-    entry: {
-      'js/index': './src/js/index.js',
-    },
-  },
-
-  spreadsheet: {
-    // Spreadsheet ID URLから引っ張る
-    id: '1SOa3TeSRJdpcuoTkoRWuhRZDEaFEzt5CJQFThPgv0Ec',
-    output: './src/spreadsheet.json',
-    sheets: [
-      // 以下サンプル なんだか知らないけど列名は小文字になる模様
-      {
-        key: 'users',
-        sheetTitle: 'ユーザ一覧',
-        rows: [['id', 'id'], ['name', '名前'], ['age', '年齢']],
-      },
-      {
-        key: 'groups',
-        sheetTitle: 'グループ',
-        rows: [['key', 'キー'], ['name', '名前']],
-      },
-    ],
-  },
-
-  browser: {
-    notify: false,
-    port: ports.browserSync,
-    proxy: `http://localhost:${ports.webpackDevServer}`,
-    reloadDebounce: 500,
-  },
+exports.browser = {
+  notify: false,
+  port: ports.browserSync,
+  proxy: `http://localhost:${ports.webpackDevServer}`,
+  reloadDebounce: 500,
 };
--- tools/gulp/_view.js
+++ tools/gulp/_view.js
@@ -1,7 +1,6 @@
 const gulp = require('gulp');
 const plumber = require('gulp-plumber');
 const ejs = require('gulp-ejs');
-const rename = require('gulp-rename');
 const htmlmin = require('gulp-htmlmin');
 const browser = require('browser-sync');
 
@@ -13,7 +12,6 @@ gulp.task('view', () =>
     .src(conf.view.src)
     .pipe(plumber())
     .pipe(ejs(siteConfig, {}, { ext: '.html' }))
-    .pipe(rename(conf.view.rename))
     .pipe(gulp.dest(conf.dest.dev))
     .pipe(browser.reload({ stream: true })),
 );
@@ -23,6 +21,5 @@ gulp.task('b.view', () =>
     .src(conf.view.src)
     .pipe(ejs(siteConfig, {}, { ext: '.html' }))
     .pipe(htmlmin())
-    .pipe(rename(conf.view.rename))
     .pipe(gulp.dest(conf.dest.build)),
 );
